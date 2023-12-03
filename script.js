a1e0f0ab8c10dbdedbc4c31f8d2313e057d8670e
ktm-kiddo <96372901+ktm-kiddo@users.noreply.github.com>
2023-12-03 09:29:12 +0000

    Github?

---
+++ script.js
@@ -0,0 +1,66 @@
+//weather API login email: funding-retries-0j@icloud.com
+// password: bykgUv-fuhpu9-diqded
+
+
+const app = Vue.createApp({
+  data() {
+    return {
+      catFact: '',
+      catPicUrl: '',
+      catFactLoading: false,
+      catPicUrlLoading: false,
+      airQuality:'Loading...',
+      currentPlace: 'Loading...',
+      currentCountry: '',
+    }
+  },
+  methods: {
+    getCatFact() {
+      this.catFactLoading = true;
+      fetch("https://catfact.ninja/fact")
+        .then(responsePhotoLink => responsePhotoLink.json())
+        .then(data => {
+          this.catFact = data.fact
+          this.catFactLoading = false;
+        })
+    },
+    getCatPhoto() {
+      this.catPicUrlLoading = true;
+      fetch("https://api.thecatapi.com/v1/images/search")
+        .then(response => response.json())
+        .then(data => {
+          this.catPicUrl = data[0].url
+          this.catPicUrlLoading = false;
+        })
+    },
+
+
+    getWeatherData() {
+      const successCallback = (position) => {
+        console.log(position);
+        fetch("https://api.weatherapi.com/v1/current.json?key=d988d52d87f943079de64335230312&q=" + position.coords.latitude + ", " + position.coords.longitude + "&aqi=yes")
+          .then(response => response.json())
+          .then (data => {
+            console.log(data)
+            this.airQuality = data.current.air_quality.pm2_5
+            this.currentPlace = data.location.name 
+            this.currentCountry = data.location.country
+          })
+      };
+      const errorCallback = (error) => {
+        console.log(error);
+      };
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback)
+      
+    }
+
+  },
+
+  mounted() {
+    console.log('mounted!')
+    this.getCatFact()
+    this.getCatPhoto()
+    this.getWeatherData()
+  }
+})
+app.mount('#app')
